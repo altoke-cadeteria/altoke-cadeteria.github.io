70f4fc249b5aabb1f6c5c2b789fd016c7746a436
altoke-cadeteria.github.io <bnico978@gmail.com>
2026-02-04 04:41:01 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('sortearBtn').addEventListener('click', function() {
-    // Genera un número aleatorio entre 1 y 250
-    const numeroSorteado = Math.floor(Math.random() * 250) + 1;
-    document.getElementById('numero').textContent = numeroSorteado;
-});
